24ce755bf164878ee5d11f795d18bf6ebab29668
unknown <@311-09.corp.college.ru>
2026-09-12 15:38:12 +0900

    добавил Program.cs

---
+++ Program.cs
@@ -0,0 +1,63 @@
+﻿
+//         Console.Write("введите ширинку прямоугольника: ");
+//         double width = double.Parse(Console.ReadLine());
+
+//         Console.Write("введите высоту прямоугольника: ");
+//         double height = double.Parse(Console.ReadLine());
+
+//         double area = width * height;
+//         double per = 2 * (width + height);
+
+//         Console.WriteLine($"площадь: {area}");
+//         Console.WriteLine($"периметр: {per}");
+
+
+//2
+
+
+//         Console.Write("введите рубли: ");
+//         double rub = double.Parse(Console.ReadLine());
+//         Console.WriteLine($"{rub / 83} долларов");
+
+
+
+//3
+
+        double sum = 0;
+        string input = Console.Write("введите температуру: ");
+        string parts = input.Split(' ');
+        List<double> chisla = new chisla<double>();
+        foreach (string i in parts)
+        {chisla.Add}
+
+        
+        Console.WriteLine($"средняя температура за неделю: {sum / 7}");
+        
+        
+//4
+
+// double per = 0;
+// double dva = 0;
+// double tr = 0;
+// for (int i = 1; i <= 5; i++)
+// {
+// Console.Write("введите оценку для первого: ");
+// per += double.Parse(Console.ReadLine());
+// }
+
+// for (int i = 1; i <= 5; i++)
+// {
+// Console.Write("введите оценку для второго: ");
+// dva += double.Parse(Console.ReadLine());
+// }
+
+// for (int i = 1; i <= 5; i++)
+// {
+// Console.Write("введите оценку для третьего: ");
+// tr += double.Parse(Console.ReadLine());
+// }
+
+// Console.WriteLine($"1: {per / 5}; 2: {dva / 5}; 3: {tr / 5}");
+
+//5
+
